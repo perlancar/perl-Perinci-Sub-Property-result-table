fa5313c326606945f544a21ebfbb927793918506
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-03-28 20:08:21 +0700

    Revise/rewrite doc, write first implementation (incomplete)

--- lib/Perinci/Sub/Property/result/table.pm
+++ lib/Perinci/Sub/Property/result/table.pm
@@ -3,6 +3,47 @@ package Perinci::Sub::Property::result::table;
 use 5.010001;
 use strict;
 use warnings;
+#use Log::Any '$log';
+
+use Perinci::Sub::PropertyUtil qw(declare_property);
+
+# VERSION
+
+declare_property(
+    name => 'result/table',
+    type => 'function',
+    schema => ['hash*'],
+    wrapper => {
+        meta => {
+            v       => 2,
+            prio    => 50,
+        },
+        handler => sub {
+            my ($self, %args) = @_;
+            my $v    = $args{new} // $args{value} // {};
+            my $meta = $args{meta};
+
+            # add result_format_options
+            {
+                last if $meta->{result_naked};
+                $self->select_section('after_call_after_res_validation');
+                $self->push_lines('# add result_format_options from result/table hints');
+                $self->push_lines('unless ($_w_res->[3] && $_w_res->[3]{result_format_options}) {');
+                $self->indent;
+                $self->push_lines(
+                    # this is a local variable, so we don't use add_var() or even _w_ prefix
+                    'my $rfo = {};',
+                    '$_w_res->[3]{result_fomat_options} = {text=>$rfo, "text-pretty"=>$rfo};',
+                );
+                $self->unindent;
+                $self->push_lines('}');
+            }
+
+            # TODO validate table data, if requested
+        },
+    },
+);
+
 
 1;
 #ABSTRACT: Specify table data in result
@@ -32,8 +73,8 @@ In function L<Rinci> metadata:
                  pos     => 2,
              },
          },
-         # allow_extra_fields => 1, # whether to allow extra fields
-         #
+         # allow_extra_fields => 0,
+         # allow_underscore_fields => 0,
      },
      ...
  }
@@ -41,8 +82,8 @@ In function L<Rinci> metadata:
 
 =head1 DESCRIPTION
 
-I often have functions that return table data, either in the form of array
-(single-column rows):
+If your function returns table data, either in the form of array (single-column
+rows):
 
  ["andi", "budi", "cinta", ...]
 
@@ -64,15 +105,118 @@ or array of hashes (with field names):
    # ...
  ]
 
-This property lets you specify the kind of table data your function returns. The
-value of this property should be a specification according to TableSpec (see
-L<SHARYANTO::TableSpec>).
+then you might want to add a C<table> property inside your C<result> property of
+your function metadata. This module offers several things:
 
-The wrapper code for this property will help add C<result_format_options>
-property to your function result metadata, so when your subroutine outputs table
-data in the console, the table will look prettier.
+=over
+
+=item *
+
+When your function is run under L<Perinci::CmdLine>, your tables will look
+prettier. This is done via adding C<result_format_options> property to your
+function result metadata, giving hints to the L<Data::Format::Pretty> formatter.
+
+=item *
 
 (NOT YET IMPLEMENTED) When you generate documentation, the table specification
 is also included in the documentation.
+
+(NOT YET IMPLEMENTED, IDEA) The user can also perhaps request the table
+specification, e.g. C<yourfunc --help=result-table-spec>, C<yourfunc
+--result-table-spec>.
+
+=item *
+
+(NOT YET IMPLEMENTED) The wrapper code can optionally validate your function
+result, making sure that your resulting table conforms to the table
+specification.
+
+=item *
+
+(NOT YET IMPLEMENTED, IDEA) The wrapper code can optionally filter, summarize,
+or sort the table on the fly before returning the final result to the user.
+
+(Alternatively, you can pipe the output to another tool like B<jq>, just like a
+la Unix toolbox philosophy).
+
+=back
+
+
+=head1 SPECIFICATION
+
+The value of the C<table> property should be a L<DefHash>. Known properties:
+
+=over
+
+=item * spec => DEFHASH
+
+Required. Table data specification, specified using L<SHARYANTO::TableSpec>.
+
+=item * allow_extra_fields => BOOL (default: 0)
+
+Whether to allow the function to return extra fields other than the ones
+specified in C<spec>. This is only relevant when function returns array of
+hashes (i.e. when the field names are present). And this is only relevant when
+validating the table data.
+
+=item * allow_underscore_fields => BOOL (default: 0)
+
+Like C<allow_extra_fields>, but regulates whether to allow any extra fields
+prefixed by an underscore. Underscore-prefixed keys
+
+=back
+
+
+=head1 NOTES
+
+If you return an array or array of arrays (i.e. no field names), you might want
+to add C<table.fields> result metadata so the wrapper code can know which
+element belongs to which field. Example:
+
+ my $table = [];
+ push @$table, ["andi", 1];
+ push @$table, ["budi", 2];
+ return [200, "OK", $table, {"table.fields"=>[qw/name id/]}];
+
+This is not needed if you return array of hashes, since the field names are
+present as hash keys:
+
+ my $table = [];
+ push @$table, {name=>"andi", id=>1};
+ push @$table, {name=>"budi", id=>2};
+ return [200, "OK", $table];
+
+
+=head1 RESULT METADATA
+
+=over
+
+=item * attribute: table.fields => ARRAY OF STR
+
+=back
+
+
+=head1 FAQ
+
+=head2 Why not use the C<schema> property in the C<result> property?
+
+That is, in your function metadata:
+
+ result => {
+     schema => ['array*', of => ['hash*' => keys => {
+         name => 'str*',
+         position => 'str',
+         salary => ['float*', min => 0],
+         ...
+     }]],
+ },
+
+First of all, table data can come in several forms, either a 1-dimensional
+array, an array of arrays, or an array of hashes. Moreover, when returning an
+array of arrays, the order of fields can sometimes be changed. The above schema
+will become more complex if it has to handle all those cases.
+
+With the C<table> property, the intent becomes clearer that we want to return
+table data. We can also specify more aspects aside from just the schema.
 
 =cut
---
+++ t/01-basics.t
@@ -0,0 +1,79 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Perinci::Sub::Wrapper qw(wrap_sub);
+use Test::More 0.98;
+use Test::Perinci::Sub::Wrapper qw(test_wrap);
+
+my $meta = {
+    v=>1.1,
+    result=>{
+        table => {
+            summary => 'Employees',
+            spec => {
+                fields => {
+                    id   => {
+                        schema => 'int*',
+                    },
+                    name => {
+                        schema => 'str*',
+                    },
+                    manager => {
+                        summary => 'Whether employee is a manager',
+                        schema => 'bool*',
+                    },
+                },
+                pk => 'id',
+            },
+        },
+    }
+};
+my $sub = sub {
+    my %args = @_;
+    my $which = $args{-which};
+    if ($which eq 'array_nohint') {
+        # return array with no table.fields hint, assumed to be pk
+        return [200, "OK", [1, 2, 3]];
+    } elsif ($which eq 'array_hint') {
+        # return array with table.fields hint
+        return [200, "OK", [qw/andi budi cinta/], {"table.fields"=>[qw/name/]}];
+    } elsif ($which eq 'aoa') {
+        return [200, "OK", [[qw/andi 1/], [qw/cinta 0/]],
+                {"table.fields"=>[qw/name manager/]}];
+    } elsif ($which eq 'aoh') {
+        return [200, "OK", [{name=>'andi', manager=>1},
+                            {name=>'cinta', manager=>0}]];
+    } elsif ($which eq 'aoh_extra') {
+        # return aoh with extra fields
+        return [200, "OK", [{name=>'andi', manager=>1, salary=>12_000_000},
+                            {name=>'cinta', manager=>0, salary=>7_500_000}]];
+    } elsif ($which eq 'aoh_underscore') {
+        # return aoh with underscore fields
+        return [200, "OK", [{name=>'andi', manager=>1, _foo=>1},
+                            {name=>'cinta', manager=>0, _bar=>2}]];
+    }
+    die "BUG: unknown -which";
+};
+test_wrap(
+    name        => 'add result_format_options',
+    wrap_args   => {sub=>$sub, meta=>$meta},
+    calls       => [
+        {
+            # currently not added
+            argsr  => [-which=>'array_nohint'],
+            result => [200, "OK", [1, 2, 3]],
+        },
+        {
+            # currently not added
+            argsr  => [-which=>'array_hint'],
+            result => [200, "OK", [qw/andi budi cinta/],
+                       {"table.fields"=>[qw/name/]}],
+        },
+    ],
+);
+
+DONE_TESTING:
+done_testing;
